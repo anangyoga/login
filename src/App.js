476b3166ec7896c7749db42af47fd1cc2924c3a0
anangyoga <aharwinto@gmail.com>
2022-12-01 23:18:03 +0700

    fix: layouts path in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "antd/dist/reset.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeLayout from "./layout/HomeLayout";
-import ProfileLayout from "./layout/ProfileLayout";
+import HomeLayout from "./layouts/HomeLayout";
+import ProfileLayout from "./layouts/ProfileLayout";
 
 function App() {
   return (
