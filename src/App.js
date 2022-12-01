2c60005bbcf530c100c0217c8b44a89a3b380c62
anangyoga <aharwinto@gmail.com>
2022-12-01 23:14:56 +0700

    feat: routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import "antd/dist/reset.css";
 import "./App.css";
-import { Col, Row } from "antd";
-import Login from "./components/Login";
-
-const homepage = {
-  background: "#BCEAD5",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-  color: "#150050",
-};
-
-const loginForm = {
-  background: "#DEF5E5",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeLayout from "./layout/HomeLayout";
+import ProfileLayout from "./layout/ProfileLayout";
 
 function App() {
-  const onFinish = (values) => {
-    console.log("Success:", values);
-  };
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
   return (
     <div>
-      <Row gutter={16} justify="center" align="center">
-        <Col flex span={12} style={homepage}>
-          <h1>Welcome to Our Homepage</h1>
-        </Col>
-        <Col span={12} style={loginForm}>
-          <Login onFinish={onFinish} onFinishFailed={onFinishFailed} />
-        </Col>
-      </Row>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeLayout />}></Route>
+          <Route path="/profile" element={<ProfileLayout />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
