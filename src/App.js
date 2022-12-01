46b1b60adc24849191bd399a78f694d65ea2b165
anangyoga <aharwinto@gmail.com>
2022-12-01 22:20:24 +0700

    feat: homepage & form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/reset.css";
+import "./App.css";
+import { Col, Row } from "antd";
+import Login from "./components/Login";
+
+const homepage = {
+  background: "#BCEAD5",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  color: "#150050",
+};
+
+const loginForm = {
+  background: "#DEF5E5",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 function App() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row gutter={16} justify="center" align="center">
+        <Col flex span={12} style={homepage}>
+          <h1>Welcome to Our Homepage</h1>
+        </Col>
+        <Col span={12} style={loginForm}>
+          <Login onFinish={onFinish} onFinishFailed={onFinishFailed} />
+        </Col>
+      </Row>
     </div>
   );
 }
