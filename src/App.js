157a1774c8dab743e4bcd6afc9a4bcd7f346c855
anangyoga <aharwinto@gmail.com>
2022-12-04 22:22:29 +0700

    fix: remove protected routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeLayout from "./layouts/HomeLayout";
 import ProfileLayout from "./layouts/ProfileLayout";
-import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/" element={<HomeLayout />}></Route>
-            <Route path="/profile" element={<ProfileLayout />} />
-          </Route>
+          <Route path="/" element={<HomeLayout />}></Route>
+          <Route path="/profile" element={<ProfileLayout />} />
         </Routes>
       </Router>
     </div>
